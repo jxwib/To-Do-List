4f48167c363feda82e5ec8592c6919f08bd3eba9
jxwib <jumanhsami@outlook.com>
2026-02-19 05:38:08 +0300

    done with the list frame

--- ToDoList/src/coponents/ToDo.jsx
+++ ToDoList/src/coponents/ToDo.jsx
@@ -3,13 +3,29 @@ import todo_icon from '../assets/todo_icon.png'
 const ToDo = () => {
   return (
     <div className='bg-orange-50 place-self-center w-11/12 max-w-md 
-    flex flex-col p-7 min-h-[550px] rounded-xl'>
+    flex flex-col p-7 min-h-137.5 rounded-xl'>
     
 {/* the title */}
+
     <div className='flex items-center mt-7 gap-2'>
         <img className= 'w-10' src={todo_icon} alt="" />
         <h1 className='text-3xl font-semibold'>To-Do List</h1>
       </div>
+
+{/* inputbox */}
+
+<div className='flex items-center my-7 bg-gray-200 rounded-full'>
+
+<input className='bg-transparent border-0 outline-none flex-1 h-14 pl-6
+pr-2 placeholder:text-slate-600' 
+type="text" placeholder='Add your task'/>
+
+<button className='border-none rounded-full bg-red-600 w-32 h-14 text-white
+text-lg font-medium Cursor-pointer'> ADD +   </button>
+
+</div>
+
+{/*  list  */}
 
 
 
---
+++ ToDoList/src/coponents/TodoItems.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import tick_icon from '../assets/tick.png'
+import not_tick_icon from '../assets/nottick.png'
+import delet_icon from '../assets/delet.png'
+import edit_icon from '../assets/Edit.png'
+
+
+const TodoItems = () => {
+  return (
+    <div className='flex items-center my-3 gap-2 '>
+      <div>
+        <img src="" alt="" />
+
+
+      </div>
+
+     
+
+    </div>
+  )
+}
+
+export default TodoItems
